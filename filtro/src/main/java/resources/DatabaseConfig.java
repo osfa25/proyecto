53aa408cf1a97fc34c05d7b7eff8a67c5c4400f1
Fabian Mantilla <153670273+osfa25@users.noreply.github.com>
2024-07-26 18:56:09 -0500

    Update DatabaseConfig.java

--- filtro/src/main/java/resources/DatabaseConfig.java
+++ filtro/src/main/java/resources/DatabaseConfig.java
@@ -11,8 +11,8 @@ public class DatabaseConfig {
 
     // local test
     private static final String URL = "jdbc:mysql://localhost:3306/sgpzf";
-    private static final String USER = "campus2023";
-    private static final String PASSWORD = "campus2023";
+    private static final String USER = "localhost";
+    private static final String PASSWORD = "123456";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
